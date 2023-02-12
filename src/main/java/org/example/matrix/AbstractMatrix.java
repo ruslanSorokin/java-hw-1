304c3c83cac3580584f815fe94e4a92a8489b4c4
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-12 11:16:22 +0300

    feat(AbstractMatrix): add 2 compatibility checks by dimensions

--- src/main/java/org/example/matrix/AbstractMatrix.java
+++ src/main/java/org/example/matrix/AbstractMatrix.java
@@ -1,6 +1,8 @@
 package org.example.matrix;
 
+import org.example.matrix.exception.IncompatibleInnerDimensionsExceptions;
 import org.example.matrix.exception.NegativeDimensionException;
+import org.example.matrix.exception.PairwiseIncompatibleDimensionsException;
 
 public abstract class AbstractMatrix {
 	protected final int _nRow;
@@ -13,6 +15,20 @@ public abstract class AbstractMatrix {
 		}
 		this._nRow = nRow;
 		this._nCol = nCol;
+	}
+
+	protected static void checkPairwiseCompatibility(AbstractMatrix lhs, AbstractMatrix rhs)
+			throws PairwiseIncompatibleDimensionsException {
+		if (lhs._nCol != rhs.getNCol() || lhs._nRow != rhs.getNRow()) {
+			throw new PairwiseIncompatibleDimensionsException(lhs, rhs);
+		}
+	}
+
+	protected static void checkInternallyCompatibility(AbstractMatrix lhs, AbstractMatrix rhs)
+			throws IncompatibleInnerDimensionsExceptions {
+		if (lhs._nCol != rhs.getNRow()) {
+			throw new IncompatibleInnerDimensionsExceptions(lhs, rhs);
+		}
 	}
 
 	public int getNRow() {
