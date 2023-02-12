7d823b1c166a6b2ea8bb6ad3f95bd95e96b1918a
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-12 13:33:49 +0300

    refactor(NegativeDimensionException): fix typos

--- src/main/java/org/example/matrix/exception/NegativeDimensionException.java
+++ src/main/java/org/example/matrix/exception/NegativeDimensionException.java
@@ -5,7 +5,7 @@ import org.example.matrix.AbstractMatrix;
 public class NegativeDimensionException extends RuntimeException {
 
 	public NegativeDimensionException(AbstractMatrix matrix) {
-		super("expected non-negative demensions:"
+		super("expected non-negative dimensions:"
 				+ String.format("%dx%d",
 						matrix.getNRow(),
 						matrix.getNCol()));
