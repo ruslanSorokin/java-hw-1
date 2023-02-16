3b89ae57a550ed3df750180d5f6fe68e2f329f6b
Ruslan Sorokin <strawberryladder@gmail.com>
2023-02-16 17:32:07 +0300

    feat(Main): add welcome function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
+import org.example.complex.util.ComplexScanner;
+import org.example.matrix.ComplexMatrix;
+import org.example.matrix.exception.NegativeDimensionException;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		var s = new Scanner(System.in);
+		var cS = new ComplexScanner(System.in);
+
+		System.out.println("Enter N:");
+		var n = s.nextInt();
+		System.out.println("Enter M:");
+		var m = s.nextInt();
+
+		try {
+			var matrix = new ComplexMatrix(n, m);
+			matrixInput(cS, matrix);
+
+			System.out.println(matrix);
+		} catch (NegativeDimensionException e) {
+			e.printStackTrace();
+		}
+
+		s.close();
+		cS.close();
 	}
+
+	static void matrixInput(ComplexScanner cS, ComplexMatrix matrix) {
+		for (int i = 0; i < matrix.getNRow(); ++i) {
+			for (int j = 0; j < matrix.getNCol(); ++j) {
+				if (!cS.hasNextComplex()) {
+					return;
+				}
+				matrix.data[i][j] = cS.nextComplex();
+			}
+		}
+	}
+
 }
